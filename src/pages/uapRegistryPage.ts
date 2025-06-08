8cc8bbf61297e860b8f8332ddd4caee1a4d3e10e
ViktorijaPidika <viktorija.pidika@gmail.com>
2025-06-08 20:23:29 +0300

    Create all automated tests

---
+++ src/pages/uapRegistryPage.ts
@@ -0,0 +1,37 @@
+import { Page, Locator } from '@playwright/test';
+
+export class UAPRegistryPage {
+
+    constructor(private page: Page) { }
+
+    get newObservationDropDown(): Locator { return this.page.locator('[data-testid="new-observation-toggle"]'); }
+    get newObservationForm(): Locator { return this.page.locator('[data-testid="new-observation-form"]'); }
+
+    get locationField(): Locator { return this.page.locator('[data-testid="new-observation-location"]'); }
+    get dateField(): Locator { return this.page.locator('[data-testid="new-observation-date"]'); }
+    get imageUrlField(): Locator { return this.page.locator('[data-testid="new-observation-image-url"]'); }
+    get descriptionField(): Locator { return this.page.locator('[data-testid="new-observation-description"]'); }
+
+    get addButton(): Locator { return this.page.locator('[data-testid="new-observation-add-button"]'); }
+
+    get observationArticle(): Locator { return this.page.locator('[data-testid="observations-container-div"] article'); }
+    get observationsContainer(): Locator { return this.page.locator('[data-testid="observations-container-div"]'); }
+
+    get observationArticleTitle(): Locator { return this.page.locator('[data-testid="observations-container-div"] article header'); }
+    get observationArticleImage(): Locator { return this.page.locator('[data-testid="observations-container-div"] article img'); }
+    get observationArticleText(): Locator { return this.page.locator('[data-testid="observations-container-div"] article p'); }
+
+
+
+    openUapRegistryPage() {
+        return this.page.goto('http://localhost:8000/');
+    }
+
+    findObservationByTitle(location: string, date: string): Locator {
+        return this.observationArticle.filter({ hasText: `${location} (${date})` });
+    }
+
+    checkPartialObservationTitle(partialTitle: string): Locator {
+        return this.observationArticle.filter({ hasText: `${partialTitle}` });
+    }
+}
---
+++ tests/specs/UAP_Registry/Add_Observation/C6_VerifyObservationCanBeAddedWithoutDescription.spec.ts
@@ -0,0 +1,53 @@
+import { test, expect } from '@playwright/test'
+import { UAPRegistryPage } from '../../../../src/pages/uapRegistryPage'
+import { fillNewObservationForm } from '../../../../src/helpers/uapRegistryHelper';
+
+test.describe('UAP Registry with missing Description input', () => {
+
+    let uapRegistryPage: UAPRegistryPage;
+    const testData = {
+        location: 'Riga, Latvia',
+        date: '2025-04-06',
+        image: 'https://i.ytimg.com/vi/Pws0Om4VquU/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLAhE0tn0U24HIjpIHKZ9H6A0ijurA',
+    };
+
+    test.beforeEach(async ({ page }) => {
+        //authentication is not present on the page. Once issue resolved:
+        //add additional log in steps here
+
+        uapRegistryPage = new UAPRegistryPage(page);
+        await uapRegistryPage.openUapRegistryPage();
+        await expect(uapRegistryPage.newObservationDropDown).toBeVisible();
+    });
+
+    test('Verify an observation can be added with the "Description" field empty', async () => {
+        await test.step('Fill in the "New Observation" form', async () => {
+            await uapRegistryPage.newObservationDropDown.click();
+            await expect(uapRegistryPage.newObservationForm).toBeVisible();
+
+            await fillNewObservationForm(uapRegistryPage, testData);
+            await uapRegistryPage.addButton.click();
+        });
+
+        await test.step('Verify new registry has been created without the description', async () => {
+            // Success message is not present upon successful submition. Once issue resolved:
+            // add additional assertation here that checks if the success message was returned
+
+            await expect(uapRegistryPage
+                .findObservationByTitle(testData.location, testData.date).last()
+            ).toBeVisible();
+
+            await expect(uapRegistryPage
+                .findObservationByTitle(testData.location, testData.date).last()
+                .locator('p')
+            ).not.toBeVisible();
+        });
+    });
+
+    test.afterEach(async ({ }) => {
+        await uapRegistryPage
+            .findObservationByTitle(testData.location, testData.date).last()
+            .locator('[value="delete"]')
+            .click();
+    });
+})
